0d92886f8e576f881872f076858da15f7bf1626e
ampyard-akshay-sushir <you@example.com>
2022-01-23 14:00:00 -0600

    Add edit

--- mongodb/models/model.js
+++ mongodb/models/model.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 
 const AppSchema = mongoose.Schema({
-    // startDate: {type: Date},
-    // endDate: {type: Date},
-    // minCount: {type: Number, minimim: 2700},
-    // maxCount: {type: Number, maximum: 3000}
-    code: {
-        type: Number
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    message: {
-        type: String
-    },
-    records: {
-        type: Array
-    }
+    startDate: {type: Date},
+    endDate: {type: Date},
+    minCount: {type: Number, minimim: 2700},
+    maxCount: {type: Number, maximum: 3000}
+    // code: {
+    //     type: Number
+    // },
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now
+    // },
+    // message: {
+    //     type: String
+    // },
+    // records: {
+    //     type: Array
+    // }
 })
 
 const response = mongoose.Schema({
